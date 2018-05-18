e66a2fd4e1dd0e1b7cd6994b29251c83332b26bf
Tammer Saleh <me@tammersaleh.com>
2018-05-18 08:30:16 -0700

    expose k8s info

--- example-web-app/app.rb
+++ example-web-app/app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    "Hello."
+    "Hello!  I'm pod #{pod} running #{version} on node #{node} with address #{address}"
   end
 
   get '/env' do
@@ -35,5 +35,25 @@ class App < Sinatra::Base
   get '/sleep' do
     sleep 10
     "Slept for 10 seconds"
+  end
+
+  def version
+    "v" + File.read('VERSION').chomp
+  end
+
+  def node
+    ENV["MY_NODE_NAME"]     # gke-lab-default-pool-125fa189-sw7x
+  end
+
+  def pod
+    ENV["MY_POD_NAME"]      # example-web-app-59dd6b459f-q29qp
+  end
+  
+  def namespace
+    ENV["MY_POD_NAMESPACE"] # default
+  end
+
+  def address
+    ENV["MY_POD_IP"]        # 10.28.0.19
   end
 end
