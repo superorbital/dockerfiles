275a93e524119e34fe21b6afd159feb35aa8b90a
Tammer Saleh <me@tammersaleh.com>
2019-01-16 19:50:21 +0000

    Tagging color at v7

--- color/color.rb
+++ color/color.rb
@@ -9,72 +9,77 @@ $stderr.sync = true
 
 ALIVE = true
 
-Signal.trap("TERM") do
-  puts "Caught a TERM signal.  Sleeping for 3 seconds and shutting down."
-  sleep 3
-  exit
-end
-
-class Color < Sinatra::Base
-  def color
-    ENV['COLOR'] || 'UNKNOWN.  Please set the $COLOR environment variable.'
+begin
+  Signal.trap("TERM") do
+    puts "Caught a TERM signal.  Sleeping for 3 seconds and shutting down."
+    sleep 3
+    exit 3
   end
 
-  if ENV['LOGFILE']
-    log = File.new(ENV['LOGFILE'], "a+")
-    $stdout.reopen(log)
-    $stderr.reopen(log)
-
-    $stderr.sync = true
-    $stdout.sync = true
-  end
-
-  get '/' do
-    return JSON.dump({color: color})
-  end
-
-  get '/env' do
-    return JSON.dump(ENV)
-  end
-
-  get '/exit' do
-    puts "Received request for /exit. Terminating."
-    File.open("/dev/termination-log", 'w') do |f|
-      f.write("Received request for /exit. Terminating.")
+  class Color < Sinatra::Base
+    def color
+      ENV['COLOR'] || 'UNKNOWN.  Please set the $COLOR environment variable.'
     end
-    Process.kill('TERM', Process.pid)
-    return "Received request for /exit.  Terminating."
+
+    if ENV['LOGFILE']
+      log = File.new(ENV['LOGFILE'], "a+")
+      $stdout.reopen(log)
+      $stderr.reopen(log)
+
+      $stderr.sync = true
+      $stdout.sync = true
+    end
+
+    get '/' do
+      return JSON.dump({color: color})
+    end
+
+    get '/env' do
+      return JSON.dump(ENV)
+    end
+
+    get '/exit' do
+      puts "Received request for /exit. Terminating."
+      File.open("/dev/termination-log", 'w') do |f|
+        f.write("Received request for /exit. Terminating.")
+      end
+      Process.kill('TERM', Process.pid)
+      return "Received request for /exit.  Terminating."
+    end
+
+    get '/sleep' do
+      sleep(10) and return "Slept for 10 seconds"
+    end
+
+    get '/hang' do
+      ::ALIVE = false
+      "Hanging..."
+    end
   end
 
-  get '/sleep' do
-    sleep(10) and return "Slept for 10 seconds"
-  end
-
-  get '/hang' do
-    ::ALIVE = false
-    "Hanging..."
-  end
-end
-
-printf "Starting up."
-ENV.fetch('STARTUP_DELAY', 0).to_i.times do |n|
-  printf "."
-  sleep 1
-end
-puts
-
-Thread.new do
-  puts "We're alive!"
-  while ::ALIVE do
+  printf "Starting up."
+  ENV.fetch('STARTUP_DELAY', 0).to_i.times do |n|
+    printf "."
     sleep 1
-    File.write("/tmp/heartbeat", "alive")
   end
-  puts "We're hung :("
-  FileUtils.rm_rf("/tmp/heartbeat")
-end
+  puts
 
-Color.run!(show_exceptions: false,
-           raise_errors: true,
-           traps: false,
-           bind: '0.0.0.0',
-           port: 80)
+  Thread.new do
+    puts "We're alive!"
+    while ::ALIVE do
+      sleep 1
+      File.write("/tmp/heartbeat", "alive")
+    end
+    puts "We're hung :("
+    FileUtils.rm_rf("/tmp/heartbeat")
+  end
+
+  Color.run!(show_exceptions: false,
+             raise_errors: true,
+             traps: false,
+             bind: '0.0.0.0',
+             port: 80)
+rescue StandardError => e
+  File.write("/dev/termination-log", e.to_s + "\n")
+  exit 2
+end
