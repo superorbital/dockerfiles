17266dab5952d907dbb92ad8e837bbfa50f1800c
Tammer Saleh <me@tammersaleh.com>
2019-01-19 00:45:42 +0000

    Tagging prism at v2

--- prism/prism.rb
+++ prism/prism.rb
@@ -8,57 +8,21 @@ Bundler.require
 $stdout.sync = true
 $stderr.sync = true
 
-ALIVE = true
-
-Signal.trap("TERM") do
-  puts "Caught a TERM signal.  Sleeping for 3 seconds and shutting down."
-  sleep 3
-  exit 3
-end
-
-COLORS=%w(
-  red
-  orange
-  yellow
-  green
-  blue
-  purple
-  brown
-  magenta
-  tan
-  cyan
-  olive
-  maroon
-  navy
-  aquamarine
-  turquoise
-  silver
-  lime
-  teal
-  indigo
-  violet
-  pink
-  black
-  white
-  gray
-)
-
 class Prism < Sinatra::Base
   get '/' do
-    return "ERROR: Please request /red, /blue, etc instead.\n"
+    return "ERROR: Please request /color/red, /color/blue, etc instead.\n"
   end
 
-  COLORS.each do |color|
-    get "/#{color}" do
-      response = HTTParty.get("http://#{color}", timeout: 5)
-      if response.code == 200
-        return response.body
-      else
-        return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
-      end
-    rescue StandardError => e
-      return JSON.pretty_generate({"error": e.message}) + "\n"
+  get "/:color" do
+    color = params[:color]
+    response = HTTParty.get("http://#{color}", timeout: ENV.fetch("TIMEOUT", 3))
+    if response.code == 200
+      return response.body
+    else
+      return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
     end
+  rescue StandardError => e
+    return JSON.pretty_generate({"error": e.message}) + "\n"
   end
 end
 
