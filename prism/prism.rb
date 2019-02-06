3ab58bb47dbb5bb9a11be6313d58bf03b6eeae48
Tammer Saleh <me@tammersaleh.com>
2019-02-06 17:48:25 +0000

    Tagging prism at v10

--- prism/prism.rb
+++ prism/prism.rb
@@ -25,18 +25,7 @@ class Prism < Sinatra::Base
 
   get "/egress" do
     puts "Requesting external URL"
-    response = HTTParty.get("http://api.ipify.org?format=json", timeout: ENV.fetch("TIMEOUT", 3))
-    if response.code == 200
-      return response.body.chomp + "\n"
-    else
-      return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
-    end
-  end
-
-  get "/:color" do
-    color = params[:color]
-    puts "Proxying to #{params[:color]}"
-    response = HTTParty.get("http://#{color}", timeout: ENV.fetch("TIMEOUT", 3))
+    response = get("http://api.ipify.org?format=json")
     if response.code == 200
       return response.body.chomp + "\n"
     else
@@ -44,6 +33,47 @@ class Prism < Sinatra::Base
     end
   rescue StandardError => e
     return JSON.pretty_generate({"error": e.message}) + "\n"
+  end
+
+  get "/*" do |color|
+    puts "Proxying to #{color}"
+    response = get("http://#{color}")
+    if response.code == 200
+      return response.body.chomp + "\n"
+    else
+      return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
+    end
+  rescue StandardError => e
+    return JSON.pretty_generate({"error": e.message}) + "\n"
+  end
+
+  private
+
+  def get(url)
+    HTTParty.get(url, timeout: timeout, headers: istio_trace_headers_from_request)
+  end
+
+  def timeout
+    ENV.fetch("TIMEOUT", 3)
+  end
+
+  def istio_trace_headers_from_request
+    names = %w(
+      x-request-id
+      x-b3-traceid
+      x-b3-spanid
+      x-b3-parentspanid
+      x-b3-sampled
+      x-b3-flags
+      x-ot-span-context
+    )
+
+    Hash[names.map { |name| [name, request_header(name)]}]
+  end
+
+  def request_header(name)
+    rackified_name = "HTTP_" + name.tr('-', '_').upcase
+    env[rackified_name]
   end
 
 end
