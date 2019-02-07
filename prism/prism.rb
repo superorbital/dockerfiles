e97432cc762e1a5c53460d590ab5589872f4e868
Tammer Saleh <me@tammersaleh.com>
2019-02-07 18:58:44 +0000

    Tagging prism at v11

--- prism/prism.rb
+++ prism/prism.rb
@@ -7,79 +7,88 @@ require 'logger'
 require 'open-uri'
 Bundler.require
 
-$stdout.sync = true
-$stderr.sync = true
+begin
+  $stdout.sync = true
+  $stderr.sync = true
 
-class Prism < Sinatra::Base
-  get '/' do
-    return "ERROR: Please request /red, /blue, etc instead.\n"
-  end
+  class Prism < Sinatra::Base
+    not_found { "Path #{request.path} is unknown." }
 
-  get "/healthz" do
-    return JSON.pretty_generate({"health": "good"}) + "\n"
-  end
-
-  get '/version' do
-    return JSON.dump({version: File.read('VERSION').chomp}) + "\n"
-  end
-
-  get "/egress" do
-    puts "Requesting external URL"
-    response = get("http://api.ipify.org?format=json")
-    if response.code == 200
-      return response.body.chomp + "\n"
-    else
-      return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
+    get '/' do
+      return "ERROR: Please request /red, /blue, etc instead.\n"
     end
-  rescue StandardError => e
-    return JSON.pretty_generate({"error": e.message}) + "\n"
-  end
 
-  get "/*" do |color|
-    puts "Proxying to #{color}"
-    response = get("http://#{color}")
-    if response.code == 200
-      return response.body.chomp + "\n"
-    else
-      return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
+    get "/healthz" do
+      return JSON.pretty_generate({"health": "good"}) + "\n"
     end
-  rescue StandardError => e
-    return JSON.pretty_generate({"error": e.message}) + "\n"
+
+    get '/version' do
+      return JSON.dump({version: File.read('VERSION').chomp}) + "\n"
+    end
+
+    get "/egress" do
+      puts "Requesting external URL"
+      response = get("http://api.ipify.org?format=json")
+      if response.code == 200
+        return response.body.chomp + "\n"
+      else
+        body = JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
+        return [response.code, body]
+      end
+    rescue StandardError => e
+      body = JSON.pretty_generate({"error": e.message}) + "\n"
+      return [500, body]
+    end
+
+    get "/*" do |color|
+      puts "Proxying to #{color}"
+      response = get("http://#{color}")
+      if response.code == 200
+        return response.body.chomp + "\n"
+      else
+        return JSON.pretty_generate({"error": response.body, "code": response.code}) + "\n"
+      end
+    rescue StandardError => e
+      return JSON.pretty_generate({"error": e.message}) + "\n"
+    end
+
+    private
+
+    def get(url)
+      HTTParty.get(url, timeout: timeout, headers: istio_trace_headers_from_request)
+    end
+
+    def timeout
+      ENV.fetch("TIMEOUT", 3)
+    end
+
+    def istio_trace_headers_from_request
+      names = %w(
+        x-request-id
+        x-b3-traceid
+        x-b3-spanid
+        x-b3-parentspanid
+        x-b3-sampled
+        x-b3-flags
+        x-ot-span-context
+      )
+
+      Hash[names.map { |name| [name, request_header(name)]}]
+    end
+
+    def request_header(name)
+      rackified_name = "HTTP_" + name.tr('-', '_').upcase
+      env[rackified_name]
+    end
+
   end
 
-  private
-
-  def get(url)
-    HTTParty.get(url, timeout: timeout, headers: istio_trace_headers_from_request)
-  end
-
-  def timeout
-    ENV.fetch("TIMEOUT", 3)
-  end
-
-  def istio_trace_headers_from_request
-    names = %w(
-      x-request-id
-      x-b3-traceid
-      x-b3-spanid
-      x-b3-parentspanid
-      x-b3-sampled
-      x-b3-flags
-      x-ot-span-context
-    )
-
-    Hash[names.map { |name| [name, request_header(name)]}]
-  end
-
-  def request_header(name)
-    rackified_name = "HTTP_" + name.tr('-', '_').upcase
-    env[rackified_name]
-  end
-
+  Prism.run!(show_exceptions: false,
+             raise_errors: true,
+             traps: false,
+             bind: '0.0.0.0',
+             port: 80)
+rescue StandardError => e
+  File.write("/dev/termination-log", e.to_s)
+  exit 2
 end
-
-Prism.run!(show_exceptions: false,
-           raise_errors: true,
-           traps: false,
-           bind: '0.0.0.0',
-           port: 80)
