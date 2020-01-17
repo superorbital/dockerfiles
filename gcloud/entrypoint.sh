0214c202a580748818b24fd3682406cf61761508
Tammer Saleh <tammer@superorbit.al>
2020-01-17 19:32:08 +0000

    Tagging gcloud at v6

--- gcloud/entrypoint.sh
+++ gcloud/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 main() {
   [[ -f /sa.json ]] || usage "Must mount service account credentials into /sa.json"
 
-  gcloud auth activate-service-account --key-file=/sa.json --quiet 2>&1 | grep -v "Activating"
+  gcloud auth activate-service-account --key-file=/sa.json --quiet 2>&1 | grep -v "Activated service account"
 
   exec "$@"
 }
